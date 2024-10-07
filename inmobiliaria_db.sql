985d7a376d5febe849010f82de4da34795782211
N1ckyto <escuderonico0@gmail.com>
2024-10-07 16:09:52 -0300

    new base

--- inmobiliaria_db.sql
+++ inmobiliaria_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2024 a las 22:19:30
+-- Tiempo de generación: 07-10-2024 a las 21:09:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,49 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `inmobiliaria_db`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `agentes`
---
-
-CREATE TABLE `agentes` (
-  `id` int(50) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `n_licencia` varchar(6) NOT NULL,
-  `a_exp` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `agentes`
---
-
-INSERT INTO `agentes` (`id`, `nombre`, `apellido`, `n_licencia`, `a_exp`) VALUES
-(1, 'Facundo', 'Sanchez', '14A72K', 4),
-(2, 'Nicolas', 'Escudero', '87J48P', 2);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `inquilinos`
---
-
-CREATE TABLE `inquilinos` (
-  `id` int(50) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `id_agente` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `inquilinos`
---
-
-INSERT INTO `inquilinos` (`id`, `nombre`, `apellido`, `id_agente`) VALUES
-(1, 'soledad', 'benitez', 2),
-(2, 'marcela', 'quiñones', 1);
 
 -- --------------------------------------------------------
 
@@ -97,34 +54,20 @@ INSERT INTO `propiedades` (`id`, `ubicacion`, `m2`, `modalidad`, `id_propietario
 CREATE TABLE `propietarios` (
   `id` int(50) NOT NULL,
   `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `id_agente` int(50) NOT NULL
+  `apellido` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `propietarios`
 --
 
-INSERT INTO `propietarios` (`id`, `nombre`, `apellido`, `id_agente`) VALUES
-(1, 'Franco', 'Espinoza', 2),
-(2, 'Lucas', 'Losano', 1);
+INSERT INTO `propietarios` (`id`, `nombre`, `apellido`) VALUES
+(1, 'Franco', 'Espinoza'),
+(2, 'Lucas', 'Losano');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `agentes`
---
-ALTER TABLE `agentes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `inquilinos`
---
-ALTER TABLE `inquilinos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_agente` (`id_agente`);
 
 --
 -- Indices de la tabla `propiedades`
@@ -137,24 +80,11 @@ ALTER TABLE `propiedades`
 -- Indices de la tabla `propietarios`
 --
 ALTER TABLE `propietarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_agente` (`id_agente`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `agentes`
---
-ALTER TABLE `agentes`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `inquilinos`
---
-ALTER TABLE `inquilinos`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `propiedades`
@@ -173,22 +103,10 @@ ALTER TABLE `propietarios`
 --
 
 --
--- Filtros para la tabla `inquilinos`
---
-ALTER TABLE `inquilinos`
-  ADD CONSTRAINT `inquilinos_ibfk_1` FOREIGN KEY (`id_agente`) REFERENCES `agentes` (`id`);
-
---
 -- Filtros para la tabla `propiedades`
 --
 ALTER TABLE `propiedades`
   ADD CONSTRAINT `propiedades_ibfk_1` FOREIGN KEY (`id_propietario`) REFERENCES `propietarios` (`id`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `propietarios`
---
-ALTER TABLE `propietarios`
-  ADD CONSTRAINT `propietarios_ibfk_1` FOREIGN KEY (`id_agente`) REFERENCES `agentes` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
